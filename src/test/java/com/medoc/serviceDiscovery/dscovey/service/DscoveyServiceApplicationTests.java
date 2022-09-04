9be8f155c02669dc4f672de42a9588b640f4bdc0
oussamaMAZ <70782166+oussama-maz2000@users.noreply.github.com>
2022-09-05 00:30:12 +0100

    ...

---
+++ src/test/java/com/medoc/serviceDiscovery/dscovey/service/DscoveyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medoc.serviceDiscovery.dscovey.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscoveyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
